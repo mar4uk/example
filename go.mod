b3b79ecaa766b52745b6a1a747ce10ea327e416c
Irina Marchuk <mar4ukira@gmail.com>
2024-10-17 13:18:12 +0100

    use hello module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module main
 
-go 1.20
+go 1.23.1
+
+require github.com/mar4uk/hello/pkg/go v0.0.0-20241017121433-bab4ad14c0ed
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+
+	app "github.com/mar4uk/hello/pkg/go"
+)
+
 func main() {
 	println("Ba dum, tss!")
+	a := app.App{
+		BundleID: "abc",
+	}
+	fmt.Println(a)
 }
